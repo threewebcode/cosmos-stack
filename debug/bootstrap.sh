b086b2f4914888fb260ec69f8f19501bb1d01dad
W3BUILD <127061691+threewebcode@users.noreply.github.com>
2023-06-27 07:16:24 +0800

    Update bootstrap.sh

--- debug/bootstrap.sh
+++ debug/bootstrap.sh
@@ -20,16 +20,26 @@ make build
 ./build/simd init mychain --home mychain --chain-id mychain
 
 ./build/simd keys add alice --home mychain --keyring-backend test
-cosmos1qm6ygukulj7xp92r4c60s76lvd0xtw74sefxn2
+# alice cosmos19wlfffyx6azhuzrpu4rl54mnddnyktzr0rt8zq
 ./build/simd keys add bob --home mychain --keyring-backend test
-cosmos129ggjn9u8h0huue2rhzvtfqrmazlz0s9cj5m75
+# bob cosmos1r0ffxtdcv3s27wvsfsck7swxt308y8qq4w5jgr
 
 ./build/simd add-genesis-account alice "1000000000stake,10000000000token" --home mychain --keyring-backend test
 ./build/simd gentx alice 1000000000stake --chain-id=mychain --home mychain --keyring-backend test
 ./build/simd collect-gentxs --home mychain
 
 ./build/simd unsafe-reset-all --home mychain
-./build/simd start --consensus.create_empty_blocks=false --home mychain 
+sed -i '357s/true/false/g' mychain/config/config.toml
+sed -i '358s/0s/5s/g' mychain/config/config.toml
+./build/simd start --consensus.create_empty_blocks=false --consensus.create_empty_blocks_interval 5s --home mychain --log_level debug
 
 ./build/simd query bank balances cosmos1qm6ygukulj7xp92r4c60s76lvd0xtw74sefxn2 --home mychain
 ./build/simd query bank balances cosmos129ggjn9u8h0huue2rhzvtfqrmazlz0s9cj5m75 --home mychain
+
+# Tendermint
+
+git clone -b v0.34.12 https://gitee.com/wonderfan/tendermint.git
+cd tendermint
+make build
+./build/tendermint init --home mychain
+./build/tendermint start --proxy_app=kvstore --home mychain --log_level=debug
